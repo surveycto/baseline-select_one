0ed3490da14f8313afb9ae92e6d57bdeba707f3f
Chuck King <cking@surveycto.com>
2020-02-14 17:22:34 -0500

    Initial support for quick appearance

--- source/script.js
+++ source/script.js
@@ -1,6 +1,6 @@
 // Find the input element
-var buttons =
-document.querySelectorAll('input[name="opt"]');
+var buttons = document.querySelectorAll('input[name="opt"]');
+var choiceContainers = document.getElementsByClassName("choice-container");
 
 // Define what happens when the user attempts to clear the response
 function clearAnswer() {
@@ -10,6 +10,10 @@ function clearAnswer() {
 // Save the user's response (update the current answer)
 function change(){
   setAnswer(this.value);
+  // If the appearance is 'quick', then also progress to the next field
+  if ( fieldProperties.APPEARANCE.includes("quick") == true ) {
+    goToNextField();
+  }
 }
 
 // When a button is pressed, call the change() function and tell it which button was pressed.
@@ -17,3 +21,12 @@ for(var i = 0; i < buttons.length; i++){
   buttons[i].onchange = change;
 }
 
+// quick appearance
+if ( fieldProperties.APPEARANCE.includes("quick") == true ) {
+  // add the appearance-quick class to all the options
+  window.onload = function() {
+    for(var i = 0; i < choiceContainers.length; i++){
+      choiceContainers[i].classList.add("appearance-quick");
+    }
+  }
+} 
