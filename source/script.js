7d190e10a16f4d3f736a98fab310f91b05408022
Max S. Haberman <max@surveycto.com>
2020-08-05 11:20:31 -0400

    Moved functions to bottom

--- source/script.js
+++ source/script.js
@@ -1,18 +1,9 @@
-/* global fieldProperties, setAnswer, goToNextField, clearAnswer */
+/* global fieldProperties, setAnswer, goToNextField */
 
 // References to the supported choice containers
 var radioButtonsContainer = document.getElementById('radio-buttons-container') // default radio buttons
 var selectDropDownContainer = document.getElementById('select-dropdown-container') // minimal appearance
 var likertContainer = document.getElementById('likert-container') // likert
-
-// Detect right-to-left languages
-function isRTL (s) {
-  var ltrChars = 'A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02B8\u0300-\u0590\u0800-\u1FFF' + '\u2C00-\uFB1C\uFDFE-\uFE6F\uFEFD-\uFFFF'
-  var rtlChars = '\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC'
-  var rtlDirCheck = new RegExp('^[^' + ltrChars + ']*[' + rtlChars + ']')
-
-  return rtlDirCheck.test(s)
-}
 
 // Prepare the current webview, making adjustments for any appearance options
 
@@ -51,35 +42,6 @@ if (fieldProperties.APPEARANCE.includes('minimal') === true) {
   }
 }
 
-// Define what happens when the user attempts to clear the response
-
-function clearAnswer () {
-  // minimal appearance
-  if (fieldProperties.APPEARANCE.includes('minimal') === true) {
-    selectDropDownContainer.value = ''
-  } else if (fieldProperties.APPEARANCE.includes('likert') === true) { // likert appearance
-    var selectedOption = document.querySelector('.likert-input-button.selected')
-    if (selectedOption) {
-      selectedOption.classList.remove('selected')
-    }
-  } else { // all other appearances
-    var selectedOption = document.querySelector('input[name="opt"]:checked')
-    if (selectedOption) {
-      selectedOption.checked = false
-      selectedOption.parentElement.classList.remove('selected')
-    }
-  }
-}
-
-// Save the user's response (update the current answer)
-
-function change () {
-  setAnswer(this.value)
-  // If the appearance is 'quick', then also progress to the next field
-  if (fieldProperties.APPEARANCE.includes('quick') === true) {
-    goToNextField()
-  }
-}
 // minimal appearance
 if (fieldProperties.APPEARANCE.includes('minimal') === true) {
   selectDropDownContainer.onchange = change // when the select dropdown is changed, call the change() function (which will update the current value)
@@ -111,13 +73,51 @@ if (fieldProperties.APPEARANCE.includes('minimal') === true) {
   }
 }
 
-// If the field label or hint contain any HTML that isn't in the form definition, then the < and > characters will have been replaced by their HTML character entities, and the HTML won't render. We need to turn those HTML entities back to actual < and > characters so that the HTML renders properly. This will allow you to render HTML from field references in your field label or hint.
-function unEntity (str) {
-  return str.replace(/&lt;/g, '<').replace(/&gt;/g, '>')
-}
 if (fieldProperties.LABEL) {
   document.querySelector('.label').innerHTML = unEntity(fieldProperties.LABEL)
 }
 if (fieldProperties.HINT) {
   document.querySelector('.hint').innerHTML = unEntity(fieldProperties.HINT)
+}
+
+// Define what happens when the user attempts to clear the response
+function clearAnswer () {
+  // minimal appearance
+  if (fieldProperties.APPEARANCE.includes('minimal') === true) {
+    selectDropDownContainer.value = ''
+  } else if (fieldProperties.APPEARANCE.includes('likert') === true) { // likert appearance
+    var selectedOption = document.querySelector('.likert-input-button.selected')
+    if (selectedOption) {
+      selectedOption.classList.remove('selected')
+    }
+  } else { // all other appearances
+    var selectedOption = document.querySelector('input[name="opt"]:checked')
+    if (selectedOption) {
+      selectedOption.checked = false
+      selectedOption.parentElement.classList.remove('selected')
+    }
+  }
+}
+
+// Save the user's response (update the current answer)
+function change () {
+  setAnswer(this.value)
+  // If the appearance is 'quick', then also progress to the next field
+  if (fieldProperties.APPEARANCE.includes('quick') === true) {
+    goToNextField()
+  }
+}
+
+// If the field label or hint contain any HTML that isn't in the form definition, then the < and > characters will have been replaced by their HTML character entities, and the HTML won't render. We need to turn those HTML entities back to actual < and > characters so that the HTML renders properly. This will allow you to render HTML from field references in your field label or hint.
+function unEntity (str) {
+  return str.replace(/&lt;/g, '<').replace(/&gt;/g, '>')
+}
+
+// Detect right-to-left languages
+function isRTL (s) {
+  var ltrChars = 'A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02B8\u0300-\u0590\u0800-\u1FFF' + '\u2C00-\uFB1C\uFDFE-\uFE6F\uFEFD-\uFFFF'
+  var rtlChars = '\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC'
+  var rtlDirCheck = new RegExp('^[^' + ltrChars + ']*[' + rtlChars + ']')
+
+  return rtlDirCheck.test(s)
 }
